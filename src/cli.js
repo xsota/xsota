58450f43dcdc6a5bf3507fc1c6619cc588aed6f4
xsota <git@xsota.com>
2019-05-20 18:39:46 +0900

    added terminalImage

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,8 @@
 'use strict';
 const path = require('path');
 const meow = require('meow');
-const termImg = require('term-img');
+const termImg = require('term-img')
+const terminalImage = require('terminal-image');
 const importJsx = require('import-jsx');
 const {h, render} = require('ink');
 
@@ -13,7 +14,11 @@ meow(`
 	  $ xsota
 `);
 
+const fallback = async () => {
+  const image = await terminalImage.file(path.join(__dirname, 'xsota.jpg'));
+  console.log(image);
+}
 
-termImg(path.join(__dirname, 'xsota.jpg'));
+termImg(path.join(__dirname, 'xsota.jpg'), { fallback });
 
 render(h(ui));
